b66b71f868714ab934b21b00291fa0ecba487ab4
catalina <catalina@catalinas-Mac-Pro.local>
2018-12-27 13:21:56 -0300

    corregido ciclo de vida de aplicacion, permisos y contexto inicializado

--- app/src/main/java/com/example/android/examen/Navigation.kt
+++ app/src/main/java/com/example/android/examen/Navigation.kt
@@ -1,23 +1,35 @@
 package com.example.android.examen
 
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.LocationManager
+import android.location.LocationManager.GPS_PROVIDER
 import android.os.Bundle
+import android.location.LocationListener
 import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
+import android.support.v4.app.ActivityCompat
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import com.google.android.gms.maps.SupportMapFragment
 import kotlinx.android.synthetic.main.activity_navigation.*
 import kotlinx.android.synthetic.main.app_bar_navigation.*
 
 class Navigation : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
+    var lm: LocationManager? = null
+    var miContexto : Context? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_navigation)
         setSupportActionBar(toolbar)
-
 
 
         val toggle = ActionBarDrawerToggle(
@@ -27,7 +39,10 @@ class Navigation : AppCompatActivity(), NavigationView.OnNavigationItemSelectedL
         toggle.syncState()
 
         nav_view.setNavigationItemSelectedListener(this)
+
     }
+
+
 
     override fun onBackPressed() {
         if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
--- app/src/main/java/com/example/android/examen/RegistroFragment.kt
+++ app/src/main/java/com/example/android/examen/RegistroFragment.kt
@@ -13,6 +13,7 @@ import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
 import android.support.v4.app.Fragment
+import android.support.v4.content.ContextCompat.getSystemService
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -53,59 +54,75 @@ class RegistroFragment : Fragment(),LocationListener, OnMapReadyCallback {
     var miContexto: Context? = null
 
 
-    override fun onMapReady(p0: GoogleMap?) {
-
-        mapa = p0
-        //se revisa nuevamente si los permisos fueron otorgados
-        val permisos = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION)
-        var granted = true
-        for (permiso in permisos) {
-            // granted = granted and (ActivityCompat.checkSelfPermission(miContexto!!, permiso) == PackageManager.PERMISSION_GRANTED)
-        }
-        if (!granted) {
-            ActivityCompat.requestPermissions(miContexto as Activity, permisos, 2)
-        } else {
-            p0?.isMyLocationEnabled = true
-        }
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        miContexto = activity
+        //Se inicializa el location manager
+        var lm = this.miContexto?.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        //El fragmento mapa necesita un callback q haremos ahora
+      //  val fragmentoMapaCB = childFragmentManager.findFragmentById(R.id.FragmentMapa) as SupportMapFragment
+      //  fragmentoMapaCB.getMapAsync(this)
+
+        //El gps es un permiso importante entonces debe salir un popup que le indique al usuario
+        //sobre el uso del GPS
+        val permisos = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION)
+        var granted = true
+        for (permiso in permisos)
+        {
+            //var miContexto : Context? = null
+            miContexto = this.activity
+            granted = granted and (ActivityCompat.checkSelfPermission(miContexto!!, permiso) == PackageManager.PERMISSION_GRANTED)
+        }
+        if (!granted)
+        {
+            miContexto = this.activity
+            ActivityCompat.requestPermissions(miContexto as Activity,permisos,1)
+        }
+        else {
+            lm?.requestLocationUpdates(LocationManager.GPS_PROVIDER,1000,1f,this)
+        }
+
+        //fragmento mapa
+       // val fragmentoMapa = childFragmentManager.findFragmentById(R.id.FragmentMapa) as SupportMapFragment
+        //fragmentoMapa.getMapAsync(this)
 
 
     }
 
     override fun onAttach(context: Context?) {
         super.onAttach(context)
-        var database = SQLiteDatabase(activity) as SQLiteDatabase
+      //  var database = SQLiteDatabase(activity) as SQLiteDatabase
     }
 
     override fun onLocationChanged(location: Location?) {
-
         latitud = location?.latitude.toString().toDouble()
         longitud = location?.longitude.toString().toDouble()
         altitud = location?.altitude.toString().toDouble()
-
         var marcador = LatLng(latitud, longitud)
         var zoom: Float = 18f
         mapa?.moveCamera(CameraUpdateFactory.newLatLngZoom(marcador, zoom))
-
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
 
-        when (requestCode) {
-            1 -> {
-                lm = miContexto?.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        miContexto = activity
+
+        when (requestCode)
+        {
+            1->
+            {
+                lm = miContexto!!.getSystemService(Context.LOCATION_SERVICE) as LocationManager
                 var granted = true
                 for (permiso in permissions) {
                     granted = granted and (ActivityCompat.checkSelfPermission(
-                        miContexto!!, permiso
-                    ) == PackageManager.PERMISSION_GRANTED)
+                        this as Activity, permiso)== PackageManager.PERMISSION_GRANTED)
                 }
-                if (grantResults.size > 0 && granted) {
-                    lm?.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1f, this)
-                } else {
+                if (grantResults.size > 0 && granted)
+                {
+                    lm?.requestLocationUpdates(LocationManager.GPS_PROVIDER,1000,1f,this)
+                } else
+                {
                     Toast.makeText(miContexto, "Permiso de gps requerido", Toast.LENGTH_LONG).show()
                 }
             }
@@ -114,56 +131,55 @@ class RegistroFragment : Fragment(),LocationListener, OnMapReadyCallback {
 
     override fun onCreateView (inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
-        // Inflate the layout for this fragment
+
+
         var v: View = inflater.inflate(R.layout.fragment_registro, container, false)
-        val fragmentoMap = childFragmentManager.findFragmentById(R.id.FragmentMapa) as SupportMapFragment?
-        fragmentoMap?.getMapAsync(this)
         var customSQL : CustomSQL = CustomSQL(miContexto, "Ubicaciones", null, 1)
-
-
-
-        // lm = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-
-        //Se inicializa el location manager
-
-        //El fragmento mapa necesita un callback q haremos ahora
-
-        val permisos = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION)
-        var granted = true
-        for (permiso in permisos) {
-       //      granted = granted and (ActivityCompat.checkSelfPermission(miContexto, permiso) == PackageManager.PERMISSION_GRANTED)
-        }
-        if (!granted) {
-            ActivityCompat.requestPermissions(miContexto as Activity, permisos, 1)
-        } else {
-     //       this.lm!!.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1f, this)
-        }
-      //  val fragmentoMapa = childFragmentManager.findFragmentById(R.id.FragmentMapa) as SupportMapFragment
-       // fragmentoMapa.getMapAsync(this)
-        this.miContexto = miContexto
+        val fragmentoMapa = childFragmentManager.findFragmentById(R.id.FragmentMapa) as SupportMapFragment
+        fragmentoMapa.getMapAsync(this)
 
         var buttonAdd = v.findViewById<Button>(R.id.btnAdd) as Button
         //var miContexto : Context = Context(miContexto)
         buttonAdd.setOnClickListener {
+
             var nombre: String = editNombre.text.toString()
             var descripcion: String = editDescripcion.text.toString()
             var marcador = LatLng(latitud, longitud)
             var zoom: Float = 20f
+
             mapa?.moveCamera(CameraUpdateFactory.newLatLngZoom(marcador, zoom))
             mapa?.addMarker(MarkerOptions().position(marcador).visible(true))
+
             var customSQL : CustomSQL = CustomSQL(miContexto, "Ubicaciones", null, 1)
-            customSQL.insertar(nombre, descripcion, latitud.toString(), longitud.toString())
-            //this.editNombre.text.clear()
-           // this.editDescripcion.text.clear()
             customSQL.getUbicaciones(nombre, descripcion, latitud.toString(), longitud.toString())
+            customSQL.insertar(nombre, descripcion, latitud.toString(), longitud.toString())
+            this.editNombre.text.clear()
+            this.editDescripcion.text.clear()
         }
-
-
         return v
     }
 
+    override fun onMapReady(p0: GoogleMap?) {
+        miContexto = this.activity
+        mapa = p0
+        //se revisa nuevamente si los permisos fueron otorgados
+        val permisos = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION)
+        var granted = true
+        for (permiso in permisos)
 
-
+        {
+            miContexto = this.activity
+            granted = granted and (ActivityCompat.checkSelfPermission(miContexto!!, permiso) == PackageManager.PERMISSION_GRANTED)
+        }
+        if (!granted)
+        {
+            //aqui se le pone codigo 2 en vez del anterior (el de mas abajo) que dice 1
+            ActivityCompat.requestPermissions(miContexto as Activity,permisos,2)
+        }
+        else {
+            p0?.isMyLocationEnabled = true
+        }
+    }
 
     override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
     }
